65cfb77c4432586a2d3e94e2904177241eaa3363
webert.cunha <webert.cunha@DESENVOLV18>
2022-01-07 10:57:34 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primeiro
+{
+
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            var a = "Hello ";
+            var b = "world";
+            Console.WriteLine(a + b);
+        }
+    }
+}
